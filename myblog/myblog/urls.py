43230c3fc62dadde9b79ccde9c1cf10bb9c29011
tuchongyang <779311998@qq.com>
2018-03-18 21:33:08 +0800

    d

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+
 import blog.views as bv
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('index/', bv.index)
+    path('index/', include('blog.urls')),
 ]
