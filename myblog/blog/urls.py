8a048cdb68effa3d57a0c927824f894b86970e58
tuchongyang <779311998@qq.com>
2018-03-20 21:54:24 +0800

    update blog

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url, include
 from . import views;
-
+app_name = 'blog'
 urlpatterns = [
 	url(r'index/', views.index),
+	url(r'^article/(?P<article_id>[\d]+)$', views.article_page, name='article'),
+	url(r'^add/(?P<article_id>[\d]+)$', views.add_page, name='article_add'),
+	url(r'^add/action$', views.edit_action, name='edit_action'),
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+
+from . import models
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello World")
+	articles = models.Article.objects.all()
+	return render(request, 'blog/index.html',{"articles":articles})
+
+
+def article_page(request, article_id):
+	article = models.Article.objects.get(pk=article_id)
+	return render(request, 'blog/article.html', {"article":article})
+
+def add_page(request, article_id):
+	if str(article_id) =='0':
+		return render(request, 'blog/add.html')
+
+	article = models.Article.objects.get(pk=article_id)
+	return render(request, 'blog/add.html', {"article": article})
+
+def edit_action(request):
+	title = request.POST.get('title', "TITLE")
+	content = request.POST.get('content', 'CONTENT')
+	article_id = request.POST.get('article_id','0')
+
+	if article_id == '0':
+		models.Article.objects.create(title=title, content=content)
+		articles = models.Article.objects.all()
+		return HttpResponseRedirect('/blog/index')
+
+	article = models.Article.objects.get(pk=article_id)
+	article.title = title
+	article.content = content
+
+	article.save()
+	return HttpResponseRedirect('/blog/index')
