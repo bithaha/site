589075985b41739e8ae9a97b199ec022a548ab3c
tuchongyang <779311998@qq.com>
2019-08-01 00:49:40 +0800

    update a

--- panduola/assets/js/common.js
+++ panduola/assets/js/common.js
@@ -1,14 +1,29 @@
+/* rem布局, 设计稿宽度1080px
+    长度rem = 设计图实际像素/100
+*/
+(function(doc, win) {
+    var docEl = doc.documentElement,
+        resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+        recalc = function() {
+            var clientWidth = docEl.clientWidth;
+            if (!clientWidth) return;
+            if(clientWidth < 600){
+                docEl.style.fontSize = 100 * document.body.clientWidth / 600 + 'px';
+            }else{
+                docEl.style.fontSize = 100 * 600 / 600 + 'px';
+            }
+        };
+
+    if (!doc.addEventListener) return;
+    win.initRem = recalc;
+    win.onresize = function(){recalc();};
+    win.addEventListener(resizeEvt, recalc, false);
+    doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
 
 
 $(function(){
-	$('.tabs').each(function(){
-		$(this).find('li').eq(0).addClass('active').siblings().removeClass('active');
-		$(this).next('.tabs-panels').find('.tabs-panel').eq(0).addClass('active').siblings().removeClass('active');
-	});
-	$(document).on('click','.tabs > li',function(){
-		$(this).addClass('active').siblings().removeClass('active');
-		$(this).parent().next().find('.tabs-panel').eq($(this).index()).addClass('active').siblings().removeClass('active');
-	});
+	
 })
 
 $(function(){
@@ -17,24 +32,3 @@ $(function(){
         $("img.lazy").lazyload({ effect : "fadeIn",data_attribute:"src"});   
     }
 })
-
-$(document).ready(function(){
-	
-	//返回顶部
-	var $backToTopTxt="",$backToTopEle = $('.gotop')
-	        .click(function() {
-	            $("html, body").animate({ scrollTop:0 }, 300);
-				
-	}), $backToTopFun = function() {	
-	        var st = $(document).scrollTop(), winh = $(window).height();
-	        (st > 150)? $backToTopEle.fadeIn(): $backToTopEle.fadeOut();    
-	        if (!window.XMLHttpRequest) {
-	            $backToTopEle.css("top", st + winh - 210);    
-	        }
-	        ofMenuTop = $('.float-menu-line').offset().top;
-	        ofMenuTop && (st+winh<=ofMenuTop)?$('.float-menu').addClass("fixed"):$('.float-menu').removeClass('fixed');
-	};
-	$(window).bind("scroll", $backToTopFun);
-	$(function() { $backToTopFun();});
-	
-});
