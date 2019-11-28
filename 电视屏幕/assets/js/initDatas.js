0c3ad2fea6868a137f10d222d8f8317fedaf2efd
tuchongyang <779311998@qq.com>
2019-11-28 22:44:29 +0800

    a

---
+++ 电视屏幕/assets/js/initDatas.js
@@ -0,0 +1,362 @@
+var initDatas = [
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站一部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 13542,
+		"payment": 1243
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 12562,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 11534,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	},
+	{
+		"department": "网站二部",
+		"avatar": "assets/images/avatar.jpg",
+		"orderNum": 30,
+		"name": "官志勇",
+		"performance": 15443,
+		"payment": 1500
+	}
+]
